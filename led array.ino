e8f4b08d4161290e8fbf62c2766ca36c8fda433f
Nazhari <32801301+Nazhari@users.noreply.github.com>
2018-09-29 11:50:11 -0400

    Add files via upload

---
+++ led array.ino
@@ -0,0 +1,28 @@
+int led[3] = {13,12,11};
+int wait = 500;
+
+void setup() {
+for(int i = 0; i < 3; i++){
+  pinMode(led[i],OUTPUT);
+}
+}
+
+void loop() {
+  while(wait >= 50){
+    for(int i = 0; i < 3; i++){
+      digitalWrite(led[i], HIGH);
+      delay(wait);
+    }
+    for(int i = 0; i < 3; i++){
+      digitalWrite(led[i], LOW);
+      delay(wait);
+    }
+    if (wait > 50){
+      wait = wait - 50;
+    }
+    else{
+      wait = 500;
+    }
+  }
+  
+}
