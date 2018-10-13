77729e2d4ba66198ecc4f161c3e0abf0d54afa4a
Nazhari <32801301+Nazhari@users.noreply.github.com>
2018-10-13 11:58:57 -0400

    Add files via upload

---
+++ go_commit_coding.ino
@@ -0,0 +1,26 @@
+int NotAViolet = 13;
+int Rose = 12;
+int Butt = 7;
+
+void setup() {
+  pinMode(Butt, INPUT);
+  digitalWrite(Butt, HIGH);
+  pinMode(NotAViolet, OUTPUT);
+  pinMode(Rose, OUTPUT);
+}
+
+void loop() {
+  if (digitalRead(Butt)== LOW){
+    digitalWrite(NotAViolet, HIGH);
+    delay(100);
+    digitalWrite(NotAViolet, LOW);
+    delay(100);
+  }
+  else{
+    digitalWrite(Rose, HIGH);
+    delay(100);
+    digitalWrite(Rose, LOW);
+    delay(100);
+  }
+  
+}
